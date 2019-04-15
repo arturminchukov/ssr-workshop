ab1b2424487d6de97212de2fe4398074634b5410
Artur.Minchukou <artur_minchukov@mail.ru>
2019-04-15 16:38:55 +0300

    Added react and recomposed webpack config

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
 import express from 'express';
-import {renderToString} from 'react-dom/server';
-import Home from './client/components/Home';
+import renderer from './helpers/renderer';
 
 const app = express();
 
-app.get('/', (req, res) => {
-    const content = renderToString(<Home />);
+app.use(express.static('public'));
 
-    res.send(content);
+app.get('/', (req, res) => {
+    res.send(renderer());
 });
 
 app.listen(3000, () => {
