63b955eec4f4c22c63a3d23a36e5f0d8b88b8880
Artur.Minchukou <artur_minchukov@mail.ru>
2019-05-01 17:08:29 +0300

    Added proxy with thunk

--- src/client/client.js
+++ src/client/client.js
@@ -5,13 +5,17 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import axios from 'axios';
 import {Provider} from 'react-redux';
 import Routes from './Routes';
 import reducers from './reducers';
 import {renderRoutes} from 'react-router-config';
 
+const axiosInstance = axios.create({
+    baseURL: '/api'
+});
 
-const store = createStore(reducers, window.INITIAL_STATE, applyMiddleware(thunk));
+const store = createStore(reducers, window.INITIAL_STATE, applyMiddleware(thunk.withExtraArgument(axiosInstance)));
 
 ReactDOM.hydrate(
     <Provider store={store}>
--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import 'babel-polyfill';
 import express from 'express';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
+import proxy from 'express-http-proxy';
 import {matchRoutes} from 'react-router-config';
 import Routes from './client/Routes';
 
 const app = express();
 
+app.use(
+    '/api',
+    proxy('http://react-ssr-api.herokuapp.com', {
+        proxyReqOptDecorator(opts) {
+            opts.headers['x-forwarded-host'] = 'localhost:3000';
+            return opts;
+        }
+    }));
 app.use(express.static('public'));
 
 app.get('*', (req, res) => {
-    const store = createStore();
-
+    const store = createStore(req);
     const promises = matchRoutes(Routes, req.path).map(({route}) => {
         return route.loadData ? route.loadData(store) : null;
     });
